2d59c7e63a52ff9d87642875db0ca8d45b370ddc
quantumDCS <2598981197@qq.com>
2023-02-14 01:07:20 +0800

    2023/2/14 Update 1

--- MainWindow.py
+++ MainWindow.py
@@ -5,7 +5,8 @@ import serial.tools.list_ports
 from datetime import datetime
 from LedWindow import LedWindow
 from WaveWindow import WaveWindow
-import codecs
+from TimeWindow import TimeWindow
+from RTCWindow import RTCWindow
 
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -16,7 +17,8 @@ class MainWindow(QtWidgets.QMainWindow):
         # 创建下级窗口
         self.led_window = LedWindow()
         self.wave_window = WaveWindow()
-
+        self.time_window = TimeWindow()
+        self.rtc_window = RTCWindow()
         # 设置窗口标题
         self.setWindowTitle('Python 上位机 未连接串口')
 
@@ -28,6 +30,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
         # 创建串口对象
         self.serial_port = serial.Serial()
+        self.received_serial_data = ""
 
         # 创建按钮
         self.switch_button = QtWidgets.QPushButton('打开串口')
@@ -35,6 +38,8 @@ class MainWindow(QtWidgets.QMainWindow):
         self.groupFrames_button = QtWidgets.QRadioButton('组帧')
         self.led_button = QtWidgets.QPushButton('灯光控制')
         self.wave_button = QtWidgets.QPushButton('波形控制')
+        self.time_button = QtWidgets.QPushButton('时间控制')
+        self.rtc_button = QtWidgets.QPushButton('RTC对时')
 
         # 创建文本
         self.port_text = QtWidgets.QLabel('串口号:')
@@ -48,17 +53,19 @@ class MainWindow(QtWidgets.QMainWindow):
         self.start_flag_edit = QtWidgets.QLineEdit()
         self.end_flag_edit = QtWidgets.QLineEdit()
 
-        # 设置默认帧头和帧尾
+        # 设置组帧
         self.start_flag_edit.setText('\x02')
         self.end_flag_edit.setText('\x03')
+        self.groupFrames_button.setChecked(True)
 
         # 创建下拉列表
         self.port_list = QtWidgets.QComboBox()
         self.baudRate_list = QtWidgets.QComboBox()
 
-        # 设置定时器
+        # 创建定时器
         self.refreshPortListTimer = QtCore.QTimer(self)
         self.receiveDataTimer = QtCore.QTimer(self)
+        self.updateTimeTimer = QtCore.QTimer(self)
 
         # 设置下拉列表内容
         self.baudRate_list.addItems(['9600', '19200', '38400', '57600', '115200'])
@@ -72,7 +79,11 @@ class MainWindow(QtWidgets.QMainWindow):
         self.led_button.setStyleSheet(
             'QPushButton {color: #ffffff; background-color: #FFA500; border-radius: 4px; padding: 5px 10px;}')
         self.wave_button.setStyleSheet(
-            'QPushButton {color: #ffffff; background-color: #EE9A00; border-radius: 4px; padding: 5px 10px;}')
+            'QPushButton {color: #ffffff; background-color: #605654; border-radius: 4px; padding: 5px 10px;}')
+        self.time_button.setStyleSheet(
+            'QPushButton {color: #ffffff; background-color: #00687C; border-radius: 4px; padding: 5px 10px;}')
+        self.rtc_button.setStyleSheet(
+            'QPushButton {color: #ffffff; background-color: #6D557E; border-radius: 4px; padding: 5px 10px;}')
 
         # 创建水平布局
         hbox1 = QtWidgets.QHBoxLayout()
@@ -107,6 +118,8 @@ class MainWindow(QtWidgets.QMainWindow):
         hbox3 = QtWidgets.QHBoxLayout()
         hbox3.addWidget(self.led_button)
         hbox3.addWidget(self.wave_button)
+        hbox3.addWidget(self.time_button)
+        hbox3.addWidget(self.rtc_button)
 
         # 创建垂直布局
         vbox = QtWidgets.QVBoxLayout()
@@ -127,8 +140,10 @@ class MainWindow(QtWidgets.QMainWindow):
         self.send_button.clicked.connect(lambda: self.send_data(self.input_text.text()))
         self.led_button.clicked.connect(self.led_control)
         self.wave_button.clicked.connect(self.wave_control)
+        self.time_button.clicked.connect(self.time_control)
+        self.rtc_button.clicked.connect(self.rtc_control)
 
-        # 连接定时器事件
+        # 设置定时器事件
         self.refreshPortListTimer.start(500)
         self.refreshPortListTimer.timeout.connect(self.refreshPortList)
 
@@ -143,7 +158,7 @@ class MainWindow(QtWidgets.QMainWindow):
 
     def switch_port(self):
         if len(serial.tools.list_ports.comports()) == 0:
-            QtWidgets.QMessageBox.critical(self, "警告", "没有检测到端口！")
+            QtWidgets.QMessageBox.critical(self, "严重警告", "没有检测到串口！")
             return
         # 如果串口未打开
         if self.serial_port.is_open is not True:
@@ -187,7 +202,11 @@ class MainWindow(QtWidgets.QMainWindow):
                 'QPushButton {color: #ffffff; background-color: #00A65A; border-radius: 4px; padding: 5px 10px;}')
 
     def send_data(self, data: str):
+        if self.serial_port.is_open is not True:
+            QtWidgets.QMessageBox.critical(self, "严重警告", "串口未打开！")
+            return
         if data == "":
+            QtWidgets.QMessageBox.warning(self, "警告", "发送数据不能为空")
             return
 
         if self.groupFrames_button.isChecked():
@@ -214,9 +233,9 @@ class MainWindow(QtWidgets.QMainWindow):
             return
         # 将数据转换为字符串
         data = self.decode_bytes(data)
-        if data != '':
-            # 将数据显示到输出框
-            self.output_text.append(datetime.now().strftime("%H:%M:%S.%f") + " <- " + data)
+        # 将数据显示到输出框
+        self.output_text.append(datetime.now().strftime("%H:%M:%S.%f") + " <- " + data)
+        self.received_serial_data = data
 
     def led_control(self):
         self.led_window.show()
@@ -225,3 +244,27 @@ class MainWindow(QtWidgets.QMainWindow):
     def wave_control(self):
         self.wave_window.show()
         self.wave_window.waveControlSignal.connect(self.send_data)
+
+    def time_control(self):
+        self.time_window.show()
+        self.time_window.timeControlSignal.connect(self.send_data)
+
+    def rtc_control(self):
+        self.rtc_window.show()
+        self.updateTimeTimer.start(50)
+        self.updateTimeTimer.timeout.connect(self._rtc_window_update_time)
+        self.rtc_window.rtcControlSignal.connect(self.send_data)
+
+    def _rtc_window_update_time(self):
+        try:
+            rtc_time = datetime.strptime(self.received_serial_data, "%y/%m/%d %H:%M:%S 星期%w\r\n")
+        except:
+            return
+        self.rtc_window.rtc_time = rtc_time
+        self.rtc_window.RTC_year_text.setText(str(rtc_time.year))
+        self.rtc_window.RTC_mouth_text.setText(str(rtc_time.month))
+        self.rtc_window.RTC_day_text.setText(str(rtc_time.day))
+        self.rtc_window.RTC_hour_text.setText(str(rtc_time.hour))
+        self.rtc_window.RTC_minute_text.setText(str(rtc_time.minute))
+        self.rtc_window.RTC_second_text.setText(str(rtc_time.second))
+        self.rtc_window.RTC_weekday_text.setText(str(rtc_time.weekday() + 1))
---
+++ RTCWindow.py
@@ -0,0 +1,188 @@
+# 导入所需的库
+from PyQt5 import QtWidgets, QtCore, QtGui
+import datetime
+from dateutil.relativedelta import relativedelta
+
+
+class RTCWindow(QtWidgets.QMainWindow):
+    # 创建RTC控制信号
+    rtcControlSignal = QtCore.pyqtSignal(str)
+
+    # 初始化函数
+    def __init__(self):
+        super().__init__()
+
+        # 设置窗口标题
+        self.setWindowTitle('RTC时间控制')
+
+        # 设置窗口图标
+        self.setWindowIcon(QtGui.QIcon('icon.png'))
+
+        # 设置窗口大小
+        self.resize(400, 150)
+
+        # 设置文本框
+        self.system_year_text = QtWidgets.QLineEdit()
+        self.system_mouth_text = QtWidgets.QLineEdit()
+        self.system_day_text = QtWidgets.QLineEdit()
+        self.system_hour_text = QtWidgets.QLineEdit()
+        self.system_minute_text = QtWidgets.QLineEdit()
+        self.system_second_text = QtWidgets.QLineEdit()
+        self.system_weekday_text = QtWidgets.QLineEdit()
+
+        self.RTC_year_text = QtWidgets.QLineEdit()
+        self.RTC_mouth_text = QtWidgets.QLineEdit()
+        self.RTC_day_text = QtWidgets.QLineEdit()
+        self.RTC_hour_text = QtWidgets.QLineEdit()
+        self.RTC_minute_text = QtWidgets.QLineEdit()
+        self.RTC_second_text = QtWidgets.QLineEdit()
+        self.RTC_weekday_text = QtWidgets.QLineEdit()
+
+        self.difference_year_text = QtWidgets.QLineEdit()
+        self.difference_mouth_text = QtWidgets.QLineEdit()
+        self.difference_day_text = QtWidgets.QLineEdit()
+        self.difference_hour_text = QtWidgets.QLineEdit()
+        self.difference_minute_text = QtWidgets.QLineEdit()
+        self.difference_second_text = QtWidgets.QLineEdit()
+        self.difference_text = QtWidgets.QLineEdit()
+
+        # 设置文本
+        self.system_time_text = QtWidgets.QLabel("系统时间:")
+        self.RTC_time_text = QtWidgets.QLabel("RTC时间:")
+        self.difference_time_text = QtWidgets.QLabel("时间相差:")
+        self.date_split_text1 = QtWidgets.QLabel("/")
+        self.date_split_text2 = QtWidgets.QLabel("/")
+        self.date_split_text3 = QtWidgets.QLabel("/")
+        self.date_split_text4 = QtWidgets.QLabel("/")
+        self.date_split_text5 = QtWidgets.QLabel("/")
+        self.date_split_text6 = QtWidgets.QLabel("/")
+        self.time_split_text1 = QtWidgets.QLabel(":")
+        self.time_split_text2 = QtWidgets.QLabel(":")
+        self.time_split_text3 = QtWidgets.QLabel(":")
+        self.time_split_text4 = QtWidgets.QLabel(":")
+        self.time_split_text5 = QtWidgets.QLabel(":")
+        self.time_split_text6 = QtWidgets.QLabel(":")
+        self.week_text1 = QtWidgets.QLabel("星期")
+        self.week_text2 = QtWidgets.QLabel("星期")
+        self.compare_text = QtWidgets.QLabel("同步")
+
+        # 创建按钮
+        self.synchronize_time_manual_button = QtWidgets.QPushButton('手动对时')
+        self.synchronize_time_auto_button = QtWidgets.QPushButton('自动对时')
+
+        # 创建水平布局
+        hbox1 = QtWidgets.QHBoxLayout()
+        hbox1.addWidget(self.system_time_text)
+        hbox1.addWidget(self.system_year_text)
+        hbox1.addWidget(self.date_split_text1)
+        hbox1.addWidget(self.system_mouth_text)
+        hbox1.addWidget(self.date_split_text2)
+        hbox1.addWidget(self.system_day_text)
+        hbox1.addWidget(self.system_hour_text)
+        hbox1.addWidget(self.time_split_text1)
+        hbox1.addWidget(self.system_minute_text)
+        hbox1.addWidget(self.time_split_text2)
+        hbox1.addWidget(self.system_second_text)
+        hbox1.addWidget(self.week_text1)
+        hbox1.addWidget(self.system_weekday_text)
+
+        hbox2 = QtWidgets.QHBoxLayout()
+        hbox2.addWidget(self.RTC_time_text)
+        hbox2.addWidget(self.RTC_year_text)
+        hbox2.addWidget(self.date_split_text3)
+        hbox2.addWidget(self.RTC_mouth_text)
+        hbox2.addWidget(self.date_split_text4)
+        hbox2.addWidget(self.RTC_day_text)
+        hbox2.addWidget(self.RTC_hour_text)
+        hbox2.addWidget(self.time_split_text3)
+        hbox2.addWidget(self.RTC_minute_text)
+        hbox2.addWidget(self.time_split_text4)
+        hbox2.addWidget(self.RTC_second_text)
+        hbox2.addWidget(self.week_text2)
+        hbox2.addWidget(self.RTC_weekday_text)
+
+        hbox3 = QtWidgets.QHBoxLayout()
+        hbox3.addWidget(self.difference_time_text)
+        hbox3.addWidget(self.difference_year_text)
+        hbox3.addWidget(self.date_split_text5)
+        hbox3.addWidget(self.difference_mouth_text)
+        hbox3.addWidget(self.date_split_text6)
+        hbox3.addWidget(self.difference_day_text)
+        hbox3.addWidget(self.difference_hour_text)
+        hbox3.addWidget(self.time_split_text5)
+        hbox3.addWidget(self.difference_minute_text)
+        hbox3.addWidget(self.time_split_text6)
+        hbox3.addWidget(self.difference_second_text)
+        hbox3.addWidget(self.compare_text)
+        hbox3.addWidget(self.difference_text)
+
+        # 创建垂直布局
+        vbox = QtWidgets.QVBoxLayout()
+        vbox.addLayout(hbox1)
+        vbox.addLayout(hbox2)
+        vbox.addLayout(hbox3)
+        vbox.addWidget(self.synchronize_time_manual_button)
+        vbox.addWidget(self.synchronize_time_auto_button)
+
+        # 将垂直布局添加到窗口
+        self.setLayout(vbox)
+
+        # 创建中心窗口
+        central_widget = QtWidgets.QWidget()
+        central_widget.setLayout(vbox)
+
+        # 将中心窗口设置为主窗口的中心窗口
+        self.setCentralWidget(central_widget)
+
+        # 连接按钮事件
+        self.synchronize_time_manual_button.clicked.connect(self.synchronize_time)
+        self.synchronize_time_auto_button.clicked.connect(self.synchronize_time_auto)
+
+        # 创建定时器
+        self.updateTimeTimer = QtCore.QTimer(self)
+        self.compareTimeTimer = QtCore.QTimer(self)
+        self.compareTimeTimer = QtCore.QTimer(self)
+
+        # 设置定时器事件
+        self.updateTimeTimer.start(500)
+        self.updateTimeTimer.timeout.connect(self.update_time)
+        self.compareTimeTimer.start(50)
+        self.updateTimeTimer.timeout.connect(self.compare_time)
+
+        self.rtc_time = datetime.datetime.now()
+
+    def update_time(self):
+        system_time = datetime.datetime.now()
+        self.system_year_text.setText(str(system_time.year))
+        self.system_mouth_text.setText(str(system_time.month))
+        self.system_day_text.setText(str(system_time.day))
+        self.system_hour_text.setText(str(system_time.hour))
+        self.system_minute_text.setText(str(system_time.minute))
+        self.system_second_text.setText(str(system_time.second))
+        self.system_weekday_text.setText(str(system_time.weekday() + 1))
+
+    def compare_time(self):
+        rtc_time = self.rtc_time
+        system_time = datetime.datetime.now()
+        difference_time = relativedelta(system_time, rtc_time)
+        self.difference_year_text.setText(str(difference_time.years))
+        self.difference_mouth_text.setText(str(difference_time.months))
+        self.difference_day_text.setText(str(difference_time.days))
+        self.difference_hour_text.setText(str(difference_time.hours))
+        self.difference_minute_text.setText(str(difference_time.minutes))
+        self.difference_second_text.setText(str(difference_time.seconds))
+        difference_widgets = [self.difference_year_text, self.difference_mouth_text, self.difference_day_text,
+                              self.difference_hour_text, self.difference_minute_text, self.difference_second_text]
+
+        if all(x.text() == "0" for x in difference_widgets):
+            self.difference_text.setText("√")
+        else:
+            self.difference_text.setText("×")
+
+    def synchronize_time(self):
+        self.rtcControlSignal.emit(datetime.datetime.now().strftime("%y/%m/%d %H:%M:%S 星期%w"))
+
+    def synchronize_time_auto(self, time: str):
+        self.compareTimeTimer.start(60000)
+        self.compareTimeTimer.timeout.connect(self.synchronize_time)
+        self.synchronize_time_auto_button.setText("已开启自动对时，每60s自动同步时间")
